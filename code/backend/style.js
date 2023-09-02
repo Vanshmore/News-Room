b4375ac6efd55c63bb74db4ddf0f4c9b204f012b
Vansh More <94687983+Vanshmore@users.noreply.github.com>
2023-09-03 01:25:41 +0530

    Add files via upload

---
+++ code/backend/style.js
@@ -0,0 +1,19 @@
+var url = "https://newsapi.org/v2/everything?q=style&apiKey=a63eff55710e40b39e62354524e0f8a3";
+var articles = null;
+styleSection = document.getElementById("styleSection");
+fetch(url)
+    .then(function(response) {
+        return response.json();
+    }).then((data)=>{
+        var articles = data.articles;
+        let newHTML = " ";        
+        for(let x = 0; x<Object.keys(articles).length; x++){
+             news = `<div class="row g-0"><div class="col-md-4">  <img src=${articles[x]["urlToImage"]} class="img-fluid rounded-start" alt="..."></div><div class="col-md-8"> <div class="card-body">   <h5 class="card-title">${articles[x]['title']}</h5>   <p class="card-text">${articles[x]["description"]}</p>   <p class="card-text"><small class="text-muted">published at:${articles[x]["publishedAt"]}</small></p> </div> </div></div>`;
+            newHTML += news;
+        }
+
+        styleSection.innerHTML = newHTML;    
+});
+
+
+
